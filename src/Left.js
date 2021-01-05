51946369263475a2afc648fbd91c81673809c544
dcbryant <cong.ding@100credit.com>
2021-01-05 15:17:59 +0800

    uppdate func name

--- src/Left.js
+++ src/Left.js
@@ -2,11 +2,11 @@ import DragComponent from './DragComponent'
 import {componentList} from './constance'
 
 function Left() {
-  const xx = (e) => {
+  const handleDragStart = (e) => {
     e.dataTransfer.setData('index', e.target.dataset.index)
   }
   return (
-    <div style={{ width: 200, border: '1px solid #ccc' }} onDragStart={xx} >
+    <div style={{ width: 200, border: '1px solid #ccc' }} onDragStart={handleDragStart} >
       {componentList.map((type, index) => (
         <DragComponent key={type} type={type} index={index} />
       ))}
